20f10c6c1d6f3bb6b51039d0e8202bc5d90a7269
Kateve52911 <kateve52911@stud.noroff.no>
2025-01-02 10:30:21 +0100

    updated hambuger menu CSS and CSS for mobile version

--- js/helpers/hamburgerMenu.js
+++ js/helpers/hamburgerMenu.js
@@ -4,12 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const closeIcon = document.querySelector(".closeIcon");
   const menuIcon = document.querySelector(".menuIcon");
 
+  // Set initial state
+  closeIcon.style.display = "none"; // Hide close icon initially
+
   hamburger.addEventListener("click", () => {
     menu.classList.toggle("open"); // Toggles visibility of menu
     const isOpen = menu.classList.contains("open");
 
     // Toggle icons
-    menuIcon.style.display = isOpen ? "none" : "block";
-    closeIcon.style.display = isOpen ? "block" : "none";
+    menuIcon.style.display = isOpen ? "none" : "block"; // Show menu icon if menu is closed
+    closeIcon.style.display = isOpen ? "block" : "none"; // Show close icon if menu is open
   });
 });
